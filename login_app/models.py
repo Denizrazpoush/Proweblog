10663f83f9a0367d6929655890f0dadfd69518ac
Denizrazpoush <denizrazpoush@gmail.com>
2022-06-10 18:44:27 +0430

    hello.

--- login_app/models.py
+++ login_app/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
+
+
+class Profile(models.Model):
+
+    title = models.CharField(max_length=20)
+    mothers_name = models.CharField(max_length=20)
+    image = models.ImageField(upload_to='images/profile')
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
